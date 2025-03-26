a0f9dec33e41c867cebd5c1099c4c75a82a5c801
llesueur2024 <leo.lesueur2024@campus-eni.fr>
2025-03-26 16:36:46 +0100

    better filters

--- src/Services/filterService.php
+++ src/Services/filterService.php
@@ -10,9 +10,13 @@ class filterService
     public function filterSorties(array $data, SortieFiltersModel $filtersObj, Participant $participant): array {
         $returnArray = [];
         foreach ($data as $sortie) {
-            if ($filtersObj->isSortieQueJOrganise() && $sortie->getOrganisateur()->getId() != $participant->getId()) {
-                continue;
-            }
+            //Sortie Organisée par l'utilisateur
+            if ($filtersObj->isSortieQueJOrganise() &&
+                $sortie->getOrganisateur()->getId() != $participant->getId()) continue;
+            //Sortie ou l'utilisateur est inscrit
+            if ($filtersObj->isSortieOuJeSuisInscrit() &&
+                !in_array($participant->getId(), array_map(fn($par): int => $par->getId(), iterator_to_array($sortie->getParticipants())))) continue;
+
             $returnArray[] = $sortie;
         }
         return $returnArray;
