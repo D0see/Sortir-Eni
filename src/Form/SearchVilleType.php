0220e105ba3eab930309e005058aeb44090f7204
Nika Bibileishvili <nikushabibileishvili91@gmail.com>
2025-03-27 16:52:43 +0100

    add filterVille

--- src/Form/SearchVilleType.php
+++ src/Form/SearchVilleType.php
@@ -2,30 +2,33 @@
 
 namespace App\Form;
 
-use App\Entity\Ville;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class VilleType extends AbstractType
+class SearchVilleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom',TextType::class,[
-                'label'=>'Ville',
-                ])
-            ->add('codePostal',TextType::class,[
-                'label'=>'Code Postal'
+            ->add('search', TextType::class, [
+                'label' => 'Le nom contient :',
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Rechercher une ville...',
+                ],
             ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        // Pas d'entité associée, c'est un formulaire purement "non mappé".
         $resolver->setDefaults([
-            'data_class' => Ville::class,
+            'data_class' => SearchVilleType::class,
+            'method' => 'GET'
         ]);
     }
 }
