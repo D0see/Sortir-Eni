c1c45639fe555732d33e95e61f55bc58de517643
Ewen ROBIC <ewen.robic2024@campus-eni.fr>
2025-04-02 11:23:31 +0200

    fix litlle things css suckkkkkkk

--- src/Form/EditProfileType.php
+++ src/Form/EditProfileType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Image;
 
-class editProfileType extends  AbstractType
+class EditProfileType extends  AbstractType
 {
     public  function  buildForm(FormBuilderInterface $builder, array $options): void
     {
